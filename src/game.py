890f010b7bf4b6e44f43760c744a7701ea462cf5
NunoLima10 <84800684+NunoLima10@users.noreply.github.com>
2023-01-10 21:59:27 -0100

    feat: class randomly robot

--- src/game.py
+++ src/game.py
@@ -2,14 +2,12 @@ from src.map import Map
 from src.robot import Robot
 from src.valueIteration_agent import ValueIterationAgent
 from src.direction import Directions
-
-from matplotlib import pyplot as plt
+from src.randomly import Randomly
 
 import pygame as pg
-import numpy as np
 import time
 class Game:
-    def __init__(self, title: str, map_path: str, cell_size: int , fps: int = 5) -> None:
+    def __init__(self, title: str, map_path: str, cell_size: int , fps: int = 120) -> None:
         self.title =  title
         self.map_path = map_path
         self.cell_size = cell_size
@@ -21,18 +19,11 @@ class Game:
         self.default_background_color = (64,64,64)
         self.setup()
 
-        self.value_iteration_agente = ValueIterationAgent(self.map)
+        # self.value_iteration_agente = ValueIterationAgent(self.map)
+        self.randomly = Randomly(self.map, self.robot, 20_000, 30)
         # for line in self.value_iteration_agente.values:
         #     print( line)
 
-        self.is_training = True
-        self.max_steps_number = 20_000
-        self.steps_number = 0
-        self.max_episode_number = 30
-        self.episode_number = 0
-        self.episode_reward = 0 
-        self.rewards =[]
-    
     @property
     def window_size(self):
         return(self.map.width, self.map.height)
@@ -77,68 +68,32 @@ class Game:
                 
 
     def update(self) -> None:
-        # self.randomly()
-        win, reward = self.robot.step(self.value_iteration_agente.get_move(self.robot.current_cell))
-        self.rewards.append(reward)
-        if win:
-            print("win heehehehehehe")
-            pg.quit()
-            # fig = plt.figure()
-            # ax = fig.add_subplot(111)
+        is_last_episode = self.randomly.update()
+        if is_last_episode:
+            exit()
+       
+        # win, reward = self.robot.step(self.value_iteration_agente.get_move(self.robot.current_cell))
+        # self.rewards.append(reward)
+        # if win:
+        #     print("win heehehehehehe")
+        #     pg.quit()
+        #     # fig = plt.figure()
+        #     # ax = fig.add_subplot(111)
 
-            # cax = ax.matshow(self.value_iteration_agente.values, interpolation='nearest', cmap='hot')
-            # fig.colorbar(cax)
+        #     # cax = ax.matshow(self.value_iteration_agente.values, interpolation='nearest', cmap='hot')
+        #     # fig.colorbar(cax)
 
-            # plt.savefig("figure2.png")
-            self.rewards.pop()
-            space = range(len(self.rewards))
-            average = np.average(self.rewards)
-            standard_deviation = np.std(self.rewards)
-            plt.plot(space,self.rewards)
-            plt.plot(space,[average for _ in space])
-            plt.savefig("figure3.png")
-        # if self.move_index < len(self.moves):
-        #     self.robot.step(self.moves[self.move_index])
-        #     self.move_index += 1
+        #     # plt.savefig("figure2.png")
+        #     self.rewards.pop()
+        #     space = range(len(self.rewards))
+        #     average = np.average(self.rewards)
+        #     standard_deviation = np.std(self.rewards)
+        #     plt.plot(space,self.rewards)
+        #     plt.plot(space,[average for _ in space])
+        #     plt.savefig("figure3.png")
+        
         pass
             
-    def randomly(self) -> None:
-        win, reward = self.robot.random_step()
-        self.steps_number += 1
-        self.episode_reward += reward
-
-        #reset
-        if win or self.steps_number > self.max_steps_number:
-            print(f"episode {self.episode_number} total reward {self.episode_reward/self.steps_number}")
-            self.rewards.append(self.episode_reward/self.steps_number)
-            self.episode_number += 1
-            self.steps_number = 0
-            self.episode_reward = 0
-            self.robot.current_cell = self.map.robot_inicial_cell
-            #end
-            if self.episode_number > self.max_episode_number:
-                time_end = time.time()
-                pg.quit()
-                plt.xlabel("episodes")
-                plt.ylabel("episode_reward/steps_number")
-                space = range(len(self.rewards))
-                average = np.average(self.rewards)
-                standard_deviation = np.std(self.rewards)
-                plt.plot(space,self.rewards)
-                plt.plot(space,[average for _ in space])
-               
-                plt.savefig("figure1.png")
-               
-
-                print("==========RandomSteps==========")
-                print(f"max steps number: {self.max_steps_number}")
-                print(f"max episode number: {self.max_episode_number}")
-                print(f"rewards: {[ round(rewards,2) for rewards in self.rewards]}")
-                print(f"average reward / steps_number: {round(average, 2)}")
-                print(f"standard_deviation reward / steps_number: {round(standard_deviation,2)}")
-                print(f"time took {round(time_end - self.time_start,2)} sec")
-                exit()
-    
     def draw(self) -> None:
         self.screen.fill(self.default_background_color)
         self.map.draw(self.screen)
---
+++ src/randomly.py
@@ -0,0 +1,70 @@
+from src.map import Map
+from src.robot import Robot
+from matplotlib import pyplot as plt
+
+
+import random
+import time
+import numpy as np
+
+class Randomly:
+    def __init__(self,map: Map, robot: Robot, max_steps_number: int,  max_episode_number: int) -> None:
+        self.map = map
+        self.robot = robot
+        self.max_steps_number = 20_000
+        self.max_episode_number = 30
+        self.steps_number = 0
+        self.episode_number = 0
+        self.episode_reward = 0 
+        self.rewards = []
+        self.space = range(max_episode_number)
+        self.average = 0
+        self.standard_deviation = 0 
+        self.start_time = time.time()
+        
+
+    def update(self) -> bool:
+        is_last_episode = False
+        win, reward = self.robot.random_step()
+    
+        self.episode_reward += reward
+        self.steps_number += 1
+        
+        if win or self.steps_number > self.max_steps_number:
+            print(f"episode {self.episode_number} total reward {self.episode_reward/self.steps_number}")
+            self.rewards.append(self.episode_reward/self.steps_number)
+            self.episode_number += 1
+            self.steps_number = 0
+            self.episode_reward = 0
+            self.robot.current_cell = self.map.robot_inicial_cell
+
+        if self.episode_number > self.max_episode_number:
+                self.calculate_statistics()
+                self.save_plot()
+                self.print_statistics()
+                is_last_episode = True
+
+        return is_last_episode
+                
+
+    def calculate_statistics(self) -> None:
+        self.rewards.pop()
+        self.time = time.time() - self.start_time
+        self.average = np.average(self.rewards)
+        self.standard_deviation = np.std(self.rewards)
+
+    def save_plot(self) -> None:
+        plt.xlabel("episodes")
+        plt.ylabel("episode_reward / steps_number")       
+        plt.plot(self.space, self.rewards)
+        plt.plot(self.space,[self.average for _ in self.space])   
+        plt.savefig("figure1.png")
+    
+    def print_statistics(self) -> None:
+        print("==========RandomSteps==========")
+        print(f"max steps number: {self.max_steps_number}")
+        print(f"max episode number: {self.max_episode_number}")
+        print(f"rewards: {[ round(rewards,2) for rewards in self.rewards]}")
+        print(f"average reward / steps_number: {round(self.average, 2)}")
+        print(f"standard_deviation reward / steps_number: {round(self.standard_deviation,2)}")
+        print(f"time took {round(self.time,2)} sec")
